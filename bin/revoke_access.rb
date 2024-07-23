49cfe05ee7031dbee10ef13a8d8f7df00002e194
William Kennedy <williamkennedyjnr@gmail.com>
2024-07-23 14:59:17 +0100

    revoke acces

--- bin/revoke_access.rb
+++ bin/revoke_access.rb
@@ -4,7 +4,7 @@ require_relative '../lib/door_keeper'
 csv_client = CsvClient.new
 
 csv_client.read_records.each do |record|
-  next if record[:granted_until].nil?
+  next if record['granted_until'].nil?
 
   door_keeper = DoorKeeper.new(record)
   door_keeper.revoke(record)
